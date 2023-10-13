5767e6c168a73736c6b005f208383e12cfe0aa50
23P31A4251 <147791825+23P31A4251@users.noreply.github.com>
2023-10-13 20:51:56 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e;     scanf("%d%d%d",&a,&b,&c);     d=c*2;     e=b+d;     if(e>=a)     printf("Qualify");     else     printf("Not Qualify");
    }

---
+++ Contest:.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d%d%d",&a,&b,&c);
+    d=c*2;
+    e=b+d;
+    if(e>=a)
+    printf("Qualify");
+    else
+    printf("Not Qualify");
+    
+}
