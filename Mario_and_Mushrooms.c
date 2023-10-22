c9ad5543dcf0bb16e527ff3c954278d8a9269b29
23P31A4251 <147791825+23P31A4251@users.noreply.github.com>
2023-10-22 08:03:04 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a%3==0)     printf("NORMAL");     else if(a%3==1)     printf("HUGE");     else if(a%3==2)     printf("SMALL"); }

--- Mario_and_Mushrooms.c
+++ Mario_and_Mushrooms.c
@@ -3,11 +3,10 @@ int main()
 {
     int a;
     scanf("%d",&a);
-    a=a%3;
-    if(a==0)
+    if(a%3==0)
     printf("NORMAL");
-    else if(a==1)
+    else if(a%3==1)
     printf("HUGE");
-    else
+    else if(a%3==2)
     printf("SMALL");
 }
