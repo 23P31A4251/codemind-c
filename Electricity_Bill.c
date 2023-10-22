57406605c8e051aab481857ee8e96d30d50f2388
23P31A4251 <147791825+23P31A4251@users.noreply.github.com>
2023-10-22 08:00:39 +0530

    #include<stdio.h> int main() {     int a;     float c,b,s,t;     scanf("%d",&a);     if(a<199)     printf("Units Consumed: %d Cost per Unit: 1.20 Bill: %.2f Surcharge: %.2f Total Amount: %.2f",a,a*1.20,0.00,a*1.20);    else if(a<400)     printf("Units Consumed: %d Cost per Unit: 1.40 Bill: %.2f Surcharge: %.2f Total Amount: %.2f",a,a*1.40,0.00,(a*1.4));      else if(a<600)     printf("Units Consumed: %d Cost per Unit: 1.60 Bill: %.2f Surcharge: %.2f Total Amount: %.2f",a,a*1.60,(15*a*1.6)/100.0,(a*1.6)+((15*a*1.60)/100.0));      else if(a<800)     printf("Units Consumed: %d Cost per Unit: 1.80 Bill: %.2f Surcharge: %.2f Total Amount: %.2f",a,a*1.80,(15*a*1.8)/100.0,(a*1.8)+((15*a*1.80)/100.0));      else     printf("Units Consumed: %d Cost per Unit: 2.00 Bill: %.2f Surcharge: %.2f Total Amount: %.2f",a,a*2.00,(15*a*2.0)/100.0,(a*2.0)+((15*a*2.00)/100.0)); }

--- Electricity_Bill.c
+++ Electricity_Bill.c
@@ -2,55 +2,36 @@
 int main()
 {
     int a;
-    float b,c,d,e,f,g,h,i,j,k,x,y,z,X,Y,A,B,C,D;
+    float c,b,s,t;
     scanf("%d",&a);
-    x=1.2;
-    y=1.4;
-    z=1.6;
-    X=1.8;
-    Y=2.0;
-    g=0;
-    h=0;
-    b=a*1.2;
-    c=a*1.4;
-    d=a*1.6;
-    e=a*1.8;
-    f=a*2.0;
-    i=(d*15)/100.0;
-    j=(e*15)/100.0;
-    k=(f*15)/100.0;
-    A=d+i;
-    B=e+j;
-    C=f+k;
     if(a<199)
     printf("Units Consumed: %d
-Cost per Unit: %.2f
+Cost per Unit: 1.20
 Bill: %.2f
 Surcharge: %.2f
-Total Amount: %.2f",a,x,b,g,b);
-    else if(a<400)
+Total Amount: %.2f",a,a*1.20,0.00,a*1.20);
+   else if(a<400)
     printf("Units Consumed: %d
-Cost per Unit: %.2f
+Cost per Unit: 1.40
 Bill: %.2f
 Surcharge: %.2f
-Total Amount: %.2f",a,y,c,h,c);
-    else if(a<600)
+Total Amount: %.2f",a,a*1.40,0.00,(a*1.4));
+     else if(a<600)
     printf("Units Consumed: %d
-Cost per Unit: %.2f
+Cost per Unit: 1.60
 Bill: %.2f
 Surcharge: %.2f
-Total Amount: %.2f",a,z,d,i,A);
-    else if(a<800)
+Total Amount: %.2f",a,a*1.60,(15*a*1.6)/100.0,(a*1.6)+((15*a*1.60)/100.0));
+     else if(a<800)
     printf("Units Consumed: %d
-Cost per Unit: %.2f
+Cost per Unit: 1.80
 Bill: %.2f
 Surcharge: %.2f
-Total Amount: %.2f",a,X,e,j,B);
-    else
+Total Amount: %.2f",a,a*1.80,(15*a*1.8)/100.0,(a*1.8)+((15*a*1.80)/100.0));
+     else 
     printf("Units Consumed: %d
-Cost per Unit: %.2f
+Cost per Unit: 2.00
 Bill: %.2f
 Surcharge: %.2f
-Total Amount: %.2f",a,Y,f,k,C);
-    
+Total Amount: %.2f",a,a*2.00,(15*a*2.0)/100.0,(a*2.0)+((15*a*2.00)/100.0));
 }
