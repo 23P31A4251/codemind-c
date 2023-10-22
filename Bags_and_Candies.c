57a7cfffe0510d7b6ab976bf0e48257d18c51ae5
23P31A4251 <147791825+23P31A4251@users.noreply.github.com>
2023-10-22 08:19:05 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e,f;     scanf("%d%d%d",&a,&b,&c);     d=c*b;     e=a/d;     if(a%d==0)     printf("%d",e);     else     printf("%d",++e); }

--- Bags_and_Candies.c
+++ Bags_and_Candies.c
@@ -3,12 +3,10 @@ int main()
 {
     int a,b,c,d,e,f;
     scanf("%d%d%d",&a,&b,&c);
-   d=b*c;
-   e=a%d;
-   f=a/d;
-   if(e==0)
-   printf("%d",f);
-   else
-   printf("%d",++f);
-   
+    d=c*b;
+    e=a/d;
+    if(a%d==0)
+    printf("%d",e);
+    else 
+    printf("%d",++e);
 }
