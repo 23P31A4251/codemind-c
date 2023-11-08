555389b044c425a139eef1a4db0ae04dc3454c95
23P31A4251 <147791825+23P31A4251@users.noreply.github.com>
2023-11-08 12:46:29 +0530

    #include<stdio.h> int main() {     int a,b,c=0,d;     scanf("%d",&a);     d=a;     while(a!=0)     {         b=a%10;         a=a/10;         c=c*10+b;     }     if(c==d)     printf("True");     else     printf("False"); }

--- Palindrome.c
+++ Palindrome.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c=0,d,e;
+    int a,b,c=0,d;
     scanf("%d",&a);
-    e=a;
-   while(a!=0)
-   {
-       b=a%10;
-       a=a/10;
-       c=c*10+b;
-   }
-   if(c==e)
-   printf("Palindrome");
-   else
-   printf("Not Palindrome");
+    d=a;
+    while(a!=0)
+    {
+        b=a%10;
+        a=a/10;
+        c=c*10+b;
+    }
+    if(c==d)
+    printf("True");
+    else
+    printf("False");
 }
