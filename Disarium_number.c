a318a3b9305ba7223ce326d52f6d609e6dfc35f1
23P31A4251 <147791825+23P31A4251@users.noreply.github.com>
2023-11-11 23:23:25 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,c,d=0,e,f,i=0;     scanf("%d",&a);     e=a;     f=a;     while(f!=0)     {        f=f/10;        i++;     }     while(e!=0)     {         b=e%10;         e=e/10;         c=pow(b,i);         d=d+c;         i--;     }     if(a==d)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Disarium_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,d=0,e,f,i=0;
+    scanf("%d",&a);
+    e=a;
+    f=a;
+    while(f!=0)
+    {
+       f=f/10; 
+       i++;
+    }
+    while(e!=0)
+    {
+        b=e%10;
+        e=e/10;
+        c=pow(b,i);
+        d=d+c;
+        i--;
+    }
+    if(a==d)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
