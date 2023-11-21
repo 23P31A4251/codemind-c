39508e4d8dfc11613e2bad05164821fbccb2de24
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-11-21 22:28:58 +0530

    #include<stdio.h> int main() {     int a,b=0,c=1,d,e,f=0;     scanf("%d",&a);     e=a;     do{         d=b+c;         b=c;         c=d;         e--;         if(a==d){             f++;             break;         }     }while(e>0);     if(f==1){         printf("True");     }     else     {         printf("False");     } }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -4,25 +4,21 @@ int main()
     int a,b=0,c=1,d,e,f=0;
     scanf("%d",&a);
     e=a;
-   do
-   {
-       d=b+c;
-       b=c;
-       c=d;
-       e--;
-       if(a==d||a==0||a==1)
-       {
-           f++;
-           break;
-       }
-       
-   }while(e>0);
-   if(f!=0)
-   {
-       printf("True");
-   }
-   else
-   {
-       printf("False");
-   }
+    do{
+        d=b+c;
+        b=c;
+        c=d;
+        e--;
+        if(a==d){
+            f++;
+            break;
+        }
+    }while(e>0);
+    if(f==1){
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
