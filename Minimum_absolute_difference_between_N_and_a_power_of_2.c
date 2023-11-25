9f1a9041b2cdf8023041ef5b54e3dc5d288163a0
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-11-25 19:02:22 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,c,d,e,i,f;     scanf("%d",&a);     for(i=1;i>0;i++)     {         b=pow(2,i);         if(b<=a)         {             c=b;         }         if(a<=b)         {             d=b;             break;         }     }     e=a-c;     f=d-a;     if(e<f)     {         printf("%d",e);     }     else     {         printf("%d",f);     } }

---
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,d,e,i,f;
+    scanf("%d",&a);
+    for(i=1;i>0;i++)
+    {
+        b=pow(2,i);
+        if(b<=a)
+        {
+            c=b;
+        }
+        if(a<=b)
+        {
+            d=b;
+            break;
+        }
+    }
+    e=a-c;
+    f=d-a;
+    if(e<f)
+    {
+        printf("%d",e);
+    }
+    else
+    {
+        printf("%d",f);
+    }
+}
