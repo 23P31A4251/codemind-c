df6c907caa3f77ed96ad5ac922f07fc7214fb46b
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-11-27 22:02:13 +0530

    #include<stdio.h> int main() {     int a,b,i,j,k;     scanf("%d",&a);     for(i=a;i>0;i--)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         for(k=a-i;k>0;k--)         {             printf("*");         }         printf(" ");     } }

---
+++ Basic_Pattern_5.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j,k;
+    scanf("%d",&a);
+    for(i=a;i>0;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        for(k=a-i;k>0;k--)
+        {
+            printf("*");
+        }
+        printf("
+");
+    }
+}
