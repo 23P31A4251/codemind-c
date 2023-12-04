3337402cb075f9765dd8e0c84a47a3c68ce06458
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-12-04 22:48:27 +0530

    #include<stdio.h> int main() {     int b,c=0,d,i;     scanf("%d",&b);     int a[b];     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);         if(a[i]%2==0)         {             c=i;         }     }     printf("%d",c); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int b,c=0,d,i;
+    scanf("%d",&b);
+    int a[b];
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2==0)
+        {
+            c=i;
+        }
+    }
+    printf("%d",c);
+}
