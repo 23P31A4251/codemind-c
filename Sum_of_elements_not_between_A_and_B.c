c9bc01b946940aa256c4574966f8cfddf1ca8619
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-12-05 11:33:20 +0530

    #include<stdio.h> int main() {     int b,c,d,i,x,y,e=0,k,j;     scanf("%d",&b);     int a[b];     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&x,&y);
        for(j=0;j<b;j++)
        {
            if(a[j]>=x&&a[j]<=y)
            {
                continue;
            }
            e=e+a[j];
        }
        printf("%d",e);
    }

---
+++ Sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int b,c,d,i,x,y,e=0,k,j;
+    scanf("%d",&b);
+    int a[b];
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&x,&y);
+   
+    for(j=0;j<b;j++)
+    {
+        if(a[j]>=x&&a[j]<=y)
+        {
+            continue;
+        }
+        e=e+a[j];
+    }
+    printf("%d",e);
+}
