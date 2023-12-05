8537a9c15b9535bc94a53f60a1c7804365050b60
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-12-05 09:51:34 +0530

    #include<stdio.h> int main() {     int b,c=0,d,i;     scanf("%d",&b);     int a[b];     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);         if(i%2==0&&a[i]%2!=0)         {             c++;         }     }     if(c==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int b,c=0,d,i;
+    scanf("%d",&b);
+    int a[b];
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+        if(i%2==0&&a[i]%2!=0)
+        {
+            c++;
+        }
+    }
+    if(c==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
