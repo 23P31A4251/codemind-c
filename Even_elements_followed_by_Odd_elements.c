b7704ae30dc465ec8fb25f86dc87559c77c04ca9
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-12-05 11:03:57 +0530

    #include<stdio.h> int main() {     int b,c=0,d,i,j;     scanf("%d",&b);     int a[b];     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);        if(a[i]%2==0)        {            printf("%d ",a[i]);        }     }
        for(j=0;j<b;j++)
        {
            if(a[j]%2!=0)
            {
                printf("%d ",a[j]);
            }
        }
        
    }

---
+++ Even_elements_followed_by_Odd_elements.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int b,c=0,d,i,j;
+    scanf("%d",&b);
+    int a[b];
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+       if(a[i]%2==0)
+       {
+           printf("%d ",a[i]);
+       }
+    }
+   
+    for(j=0;j<b;j++)
+    {
+        if(a[j]%2!=0)
+        {
+            printf("%d ",a[j]);
+        }
+    }
+    
+}
