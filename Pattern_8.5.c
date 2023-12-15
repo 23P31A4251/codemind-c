d206bbfcd12cccc7837f5aa9bd182bc8a2e02aab
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-12-15 10:26:03 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     int b=65;     for(i=1;i<=a;i++)     {         for(j=1;j<=a;j++)         {             if(j==i||j==a-i+1)             {                 printf("%c",b);             }             else             {                 printf(" ");             }         }         b++;         printf(" ");     } }

---
+++ Pattern_8.5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    int b=65;
+    for(i=1;i<=a;i++)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(j==i||j==a-i+1)
+            {
+                printf("%c",b);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        b++;
+        printf("
+");
+    }
+}
