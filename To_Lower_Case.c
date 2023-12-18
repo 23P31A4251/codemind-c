8b7299d6cdaa2440b8e0f01d1fa7d2bf18695275
DHANUSH NAGISETTI <147791825+23P31A4251@users.noreply.github.com>
2023-12-18 06:45:17 +0530

    #include<stdio.h> int main() {     char str[1000];     int a,i;     scanf("%[^ ]",&str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>=65&&str[i]<=95)         {             str[i]=str[i]+32;         }     }     printf("%s",str); }

---
+++ To_Lower_Case.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str[1000];
+    int a,i;
+    scanf("%[^
+]",&str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>=65&&str[i]<=95)
+        {
+            str[i]=str[i]+32;
+        }
+    }
+    printf("%s",str);
+}
